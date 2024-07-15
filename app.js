9163c434e9d66ade734b75ffffa5b60c4400dcc0
steven <sseller6@byui.edu>
2024-07-15 14:21:49 -0600

    Finished UX of results.html

--- app.js
+++ app.js
@@ -6,25 +6,3 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active');
 })
 
-document.getElementById("calculate-average").addEventListener("click", function() {
-    var questions = ["overall-experience", "value", "expectations", "ease-of-use", "recommend"];
-    var total = 0;
-    var count = 0;
-
-    questions.forEach(function(question) {
-        var answers = document.getElementsByName(question);
-        for (var i = 0; i < answers.length; i++) {
-            if (answers[i].checked) {
-                total += parseInt(answers[i].value);
-                count++;
-            }
-        }
-    });
-
-    if (count > 0) {
-        var average = total / count;
-        alert("The average rating is: " + average.toFixed(2));
-    } else {
-        alert("Please answer all the questions.");
-    }
-})
