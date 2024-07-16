84ddbe0c8bdf3c1c83f2e1b6d84f3107ec65686d
steven <sseller6@byui.edu>
2024-07-16 15:05:19 -0600

    Finished Review Request requirement.

--- app_results.js
+++ app_results.js
@@ -22,6 +22,10 @@ const role = urlParams.get('role')
 console.log(role);
 // The role of the user.
 
+const company = urlParams.get('company')
+console.log(company);
+// The company that the user is rating.
+
 const userExperience = parseInt(urlParams.get('userExperience'))
 console.log(userExperience);
 // The userExperience of the user.
@@ -53,5 +57,19 @@ console.log(average)
 if (average == 5)
 {
     let prompt = document.querySelector("#prompt")
-    prompt.innerHTML = "GO TO GOOGLE NOW"
+    prompt.innerHTML = "Thanks for the 5 star rating! Would you like to leave a Yelp Review?"
+
+    let direct_button = document.querySelector("#direct_button")
+    direct_button.innerHTML = "Go to Yelp"
+    if (company == "revive")
+    {
+        direct_button.href = "https://www.yelp.com/biz/revive-roofing-and-construction-newton"
+        direct_button.target = "_blank"
+    }
+    else if (company == "serve")
+    {
+        direct_button.href = "https://www.yelp.com/biz/serve-pest-control-provo-2"
+        direct_button.target = "_blank"
+    }
+
 }
